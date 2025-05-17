7759da09ea74cc7d19529321a2e5d6abe56f644e
batvin3211 <104458965+batvin3211@users.noreply.github.com>
2025-05-17 16:55:00 +0000

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,7 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux procps iproute dnsmasq iptables-nft hostapd lw-cli haveged NetworkManager-wifi iwlwifi-dvm-firmware
+dnf5 install -y hostapd lw-cli haveged NetworkManager-wifi iwlwifi-dvm-firmware
 
 # Use a COPR Example:
 #
